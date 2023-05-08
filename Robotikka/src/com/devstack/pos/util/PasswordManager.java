e11e16063acffb3fb713210ed7954cd4dfe8aea6
LakshithaMaldeniya <rslakshithamaldeniya@gmail.com>
2023-05-09 00:28:44 +0530

    first commit

---
+++ Robotikka/src/com/devstack/pos/util/PasswordManager.java
@@ -0,0 +1,13 @@
+package com.devstack.pos.util;
+
+import org.mindrot.BCrypt;
+
+public class PasswordManager {
+    public static String encryptPassword(String plainPassword){
+        return BCrypt.hashpw(plainPassword,BCrypt.gensalt(10));
+    }
+
+    public static boolean checkPassword(String plainText,String hash){
+        return BCrypt.checkpw(plainText,hash);
+    }
+}
